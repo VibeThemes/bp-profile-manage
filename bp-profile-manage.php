678753dd3b43426f6bcd2d19830dcf7b00181498
99Fusion-PC\99Fusion <rohit@99fusion.com>
2017-05-15 16:42:27 +0530

    Initialise

---
+++ bp-profile-manage.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: BP Profile Manage
+ * Plugin URI:  https://vibethemes.com
+ * Description: Add, remove, reorder and manage custom sections from your buddypress profiles
+ * Author:      Vibethemes,
+ * Author URI:  https://vibethemes.com
+ * Version:     1.0
+ * Text Domain: bpm
+ * Domain Path: /languages/
+ * License:     GPLv2 or later (license.txt)
+ */
